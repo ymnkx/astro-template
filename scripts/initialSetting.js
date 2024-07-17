259145d81ab630c91da6d50d714ab00f14cd356c
Yamanaka Tomohiro <yamanaka@un-t.com>
2024-07-17 15:36:49 +0900

    初期設定にデフォルト値を追加

--- scripts/initialSetting.js
+++ scripts/initialSetting.js
@@ -3,7 +3,8 @@ import fs from 'node:fs';
 
 const packageJsonFile = 'package.json';
 const packageJsonData = JSON.parse(fs.readFileSync(packageJsonFile, 'utf8'));
+const currentName = packageJsonData.name;
 
-const projectName = await input({ message: 'プロジェクト名を半角英数字で入力してください：' });
-packageJsonData.name = projectName ? projectName : 'project-name';
+const projectName = await input({ message: 'プロジェクト名を半角英数字で入力してください：', default: currentName });
+packageJsonData.name = projectName ? projectName : currentName;
 fs.writeFileSync(packageJsonFile, JSON.stringify(packageJsonData, null, 2));
