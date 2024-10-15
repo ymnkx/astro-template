d17c1240630befc06214e19d70a453483f723ec3
Yamanaka Tomohiro <yamanaka@un-t.com>
2024-10-15 12:37:08 +0900

    SassのAPIが新しくなる準備

--- astro.config.mjs
+++ astro.config.mjs
@@ -55,5 +55,12 @@ export default defineConfig({
         },
       },
     },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
+      },
+    },
   },
 });
